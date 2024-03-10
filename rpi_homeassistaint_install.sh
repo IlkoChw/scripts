48ecd84f32c5173d6870601a26a6bbe4db21e582
ilkochw <ilkochw@gmail.com>
2024-03-10 13:29:36 +0300

    added HA

--- rpi_homeassistaint_install.sh
+++ rpi_homeassistaint_install.sh
@@ -2,5 +2,5 @@
 
 sudo mkdir -p /opt/services/homeassistain
 ls /opt/services/homeassistain
-curl -fsSL https://raw.githubusercontent.com/IlkoChw/scripts/master/compose/homeassistain.yml -o docker-compose.yml
+curl -fsSL https://raw.githubusercontent.com/IlkoChw/scripts/master/compose/homeassistaint.yml -o docker-compose.yml
 sudo docker-compose up --build -d
