10aa9a20b29c6eb7cbffebe8e4f3e749c793f39b
ilkochw <ilkochw@gmail.com>
2024-03-15 22:11:20 +0300

    fixed ha

--- rpi_homeassistaint_install.sh
+++ rpi_homeassistaint_install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 sudo mkdir -p /opt/services/homeassistaint
-# shellcheck disable=SC2164
 cd /opt/services/homeassistaint
 curl -fsSL https://raw.githubusercontent.com/IlkoChw/scripts/master/compose/homeassistaint.yml -o docker-compose.yml
 sudo docker-compose up --build -d
